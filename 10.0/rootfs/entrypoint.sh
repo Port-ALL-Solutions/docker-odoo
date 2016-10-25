1582303baf0bfaf0e5786bcc00b9e729b27e464b
Benoît Vézina <benoit@vezina.biz>
2016-10-24 21:57:20 -0400

    Odoo 10 working

--- 10.0/rootfs/entrypoint.sh
+++ 10.0/rootfs/entrypoint.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-
 set -e
+
+# Replace {{ ENV }} vars
+_envtpl() {
+  mv "$1" "$1.tpl" # envtpl requires files to have .tpl extension
+  envtpl "$1.tpl"
+}
+
+#_envtpl /etc/odoo/odoo.conf
 
 # set odoo database host, port, user and password
 : ${PGHOST:=$DB_PORT_5432_TCP_ADDR}
 : ${PGPORT:=$DB_PORT_5432_TCP_PORT}
 : ${PGUSER:=${DB_ENV_POSTGRES_USER:='postgres'}}
 : ${PGPASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
-export PGHOST PGPORT PGUSER PGPASSWORD
 
 case "$1" in
 	--)
 		shift
-		exec openerp-server "$@"
+		exec odoo "$@"
 		;;
 	-*)
-		exec openerp-server "$@"
+		exec odoo "$@"
 		;;
 	*)
 		exec "$@"
 esac
 
 exit 1
+
